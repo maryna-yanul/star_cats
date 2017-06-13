dedb3f37d656fd4fde91ca8ac67d418a2ee75d75
maryna.yanul@gmail.com <maryna.yanul@gmail.com>
2017-06-13 17:29:25 +0300

    Tried to make facebook authentication.

--- src/Login.vue
+++ src/Login.vue
@@ -3,8 +3,8 @@
     <h1 class="title" v-if="this.registration">{{textRegistration}}</h1>
     <h1 class="title" v-else>{{textLogin}}</h1>
     <div class="soc-icons">
-      <span class="facebook" v-on:click="userSignOut()" />
-      <span class="google" />
+      <span class="facebook" v-on:click="facebookAuth()" />
+      <span class="google" v-on:click="userSignOut()" />
     </div>
     <form class="login-form">
       <input
@@ -50,6 +50,31 @@
       }
     },
     methods: {
+      facebookAuth() {
+        const provider = new firebase.auth.FacebookAuthProvider();
+        provider.setCustomParameters({
+          'display': 'popup'
+        });
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+
+        console.log(provider.addScope('user_birthday'));
+
+      },
       toggleAuthFormRegistration() {
         this.registration = true;
       },
@@ -89,7 +114,6 @@
         });
         firebase.auth().onAuthStateChanged(user => {
           if (user) {
-            console.log(user)
             this.$router.push({ path: '/choose-planet' });
           }
         });
